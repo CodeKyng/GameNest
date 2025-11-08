23094a01e9808f8a558ff3fa1a83d976d612dfbb
CodeKyng <jamesemmanuel0jonimi123@gmail.com>
2025-11-08 09:37:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Smooth scroll to Games section
+document.getElementById('exploreBtn').addEventListener('click', () => {
+  document.getElementById('games').scrollIntoView({ behavior: 'smooth' });
+});
+
